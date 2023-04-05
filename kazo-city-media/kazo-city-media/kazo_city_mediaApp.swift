e014822e18af60d0466fb26a97533110dec0618b
= <=>
2023-04-05 21:49:18 +0900

    create app project

---
+++ kazo-city-media/kazo-city-media/kazo_city_mediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kazo_city_mediaApp.swift
+//  kazo-city-media
+//
+//  Created by shuya on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct kazo_city_mediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kazo-city-media/kazo-city-mediaUITests/kazo_city_mediaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  kazo_city_mediaUITestsLaunchTests.swift
+//  kazo-city-mediaUITests
+//
+//  Created by shuya on 2023/04/05.
+//
+
+import XCTest
+
+final class kazo_city_mediaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
