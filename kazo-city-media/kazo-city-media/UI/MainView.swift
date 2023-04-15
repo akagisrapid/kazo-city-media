115bb44ca233a0cf78b3b553622ffb3cbf9c305e
shuya <akagisrapid@gmail.com>
2023-04-15 11:54:01 +0900

    ボタン調整

--- kazo-city-media/kazo-city-media/UI/MainView.swift
+++ kazo-city-media/kazo-city-media/UI/MainView.swift
@@ -9,57 +9,60 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
+        
         NavigationStack{
             HStack{
                 Button(action:{}){
                     Text("新着News")
                         .fontWeight(.semibold)
-                        .frame(width: 80, height: 40)
-                        .cornerRadius(20)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
+                        .padding()
                 }
                 .overlay(
                     RoundedRectangle(
-                        cornerRadius: 20)
+                        cornerRadius: 30)
                     .stroke(Color.blue,lineWidth: 1)
                 )
                 
-                    Button(action:{}){
-                        Text("Category")
-                            .fontWeight(.semibold)
-                            .frame(width: 80, height: 40)
-                            .cornerRadius(20)
-                    }
-                    .overlay(
-                        RoundedRectangle(
-                            cornerRadius: 20)
-                        .stroke(Color.blue,lineWidth: 1)
-                    )
+                Button(action:{}){
+                    Text("Category")
+                        .fontWeight(.semibold)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
+                        .padding()
+                }
+                .overlay(
+                    RoundedRectangle(
+                        cornerRadius: 30)
+                    .stroke(Color.blue,lineWidth: 1)
+                )
                 
-                    Button(action:{}){
-                        Text("Feature")
-                            .fontWeight(.semibold)
-                            .frame(width: 80, height: 40)
-                            .cornerRadius(20)
-                    }
-                    .overlay(
-                        RoundedRectangle(
-                            cornerRadius: 20)
-                        .stroke(Color.blue,lineWidth: 1)
-                    )
-                
-                    Button(action:{}){
-                        Text("Writer's")
-                            .fontWeight(.semibold)
-                            .frame(width: 80, height: 40)
-                            .cornerRadius(20)
-                    }
-                    .overlay(
-                        RoundedRectangle(
-                            cornerRadius: 20)
-                        .stroke(Color.blue,lineWidth: 1)
-                    )
+                Button(action:{}){
+                    Text("Feature")
+                        .fontWeight(.semibold)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
+                        .padding()
+                }
+                .overlay(
+                    RoundedRectangle(
+                        cornerRadius: 30)
+                    .stroke(Color.blue,lineWidth: 1)
+                )
+                Button(action:{}){
+                    Text("Writer's")
+                        .fontWeight(.semibold)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
+                        .padding()
+                }
+                .overlay(
+                    RoundedRectangle(
+                        cornerRadius: 30)
+                    .stroke(Color.blue,lineWidth: 1)
+                )
             }
-            
             List{
                 LatestNewsView()
                 CategorySelectView()
@@ -68,6 +71,7 @@ struct MainView: View {
                 MylistSelectView()
             }
             .navigationTitle("かぞっこ")
+            
             .navigationBarTitleDisplayMode(.inline)
             .toolbar{
                 
