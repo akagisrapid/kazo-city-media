6cf366bf6c8ddf63aab8bfa93a28ef3b2256bc50
shuya <akagisrapid@gmail.com>
2023-05-10 00:11:53 +0900

    overlayを使うよう修正

--- kazo-city-media/kazo-city-media/UI/CategoryView.swift
+++ kazo-city-media/kazo-city-media/UI/CategoryView.swift
@@ -11,25 +11,26 @@ struct CategoryView: View {
     @Binding var categoryModel: CategoryModel
     
     var body: some View {
-        ZStack(alignment: .bottom){
-            Image(categoryModel.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 200, height: 300)
-                .clipped()
-            VStack(alignment: .leading, spacing: 8){
-                Text(categoryModel.name)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .fixedSize(horizontal: true, vertical: false)
+        Image(categoryModel.imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 200, height: 300)
+            .clipped()
+            .overlay{
+                VStack {
+                    Spacer()
+                    Text(categoryModel.name)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                }
             }
-            .padding()
-        }
     }
+    
 }
 
 struct CategoryView_Previews: PreviewProvider {
-    @State static var categoryModel: CategoryModel = CategoryModel(name: "おでかけ・スポット", imageName: "odekakeSpot")
+    @State static var categoryModel: CategoryModel = categories.first ?? CategoryModel(name: "おでかけ・スポット", imageName: "odekakeSpot")
     
     
     static var previews: some View {
