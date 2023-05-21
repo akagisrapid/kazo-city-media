00d5e520623b78fdb69792410fe18c347e2d169e
shuya <akagisrapid@gmail.com>
2023-05-21 12:10:42 +0900

    featuresカード作成

--- kazo_city_media/lib/main.dart
+++ kazo_city_media/lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:kazo_city_media/UI/category.dart';
+import 'package:kazo_city_media/UI/feature.dart';
 
 // void main() {
 //   runApp(const MyApp());
@@ -47,6 +48,20 @@ class _MyHomePageState extends State<MyHomePage> {
     CategoryModel(name: "美容室・サロン", imageUri: 'images/salon.png'),
     CategoryModel(name: "美容室・サロン", imageUri: 'images/salon.png'),
     CategoryModel(name: "美容室・サロン", imageUri: 'images/salon.png'),
+  ];
+  var features = [
+    FeatureModel(title: "3県境の秘密に密着取材！", imageUri: 'images/kyoukaigui.png', name: "角田 守良", belonging: "市長",
+       comment: '未来へ受け継がれる歴史を継承し、\n市民のプライドを守りたい',
+    backgroundColor: Color.fromARGB(255, 206, 253, 0)
+    ),
+    FeatureModel(title: "経営戦略をのぞいてみた。", imageUri: "images/ferris_wheel.png", name: "山本 朱里", belonging: "社長",
+     comment: "子供の頃楽しかった思い出を\n今の世代にも体験して欲しかった",
+     backgroundColor: Color.fromARGB(255, 42, 36, 108)
+     ),
+    FeatureModel(title: "初主演の舞台裏へ。", imageUri: "images/ryohei_seki.png", name: "関 涼平", belonging: "俳優", 
+    comment: "人生には誰にでもドラマがある。\n偶然、今回の役と自分が重なった",
+    backgroundColor: Color.fromARGB(0, 11, 11, 11)
+    ),
   ];
 
   @override
@@ -130,7 +145,30 @@ class _MyHomePageState extends State<MyHomePage> {
                               .map((e) => CategoryItem(category: e))
                               .toList())))
             ],
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text('FEATURE（特集・注目）',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ))),
+              Scrollbar(
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                          children: features
+                              .map((e) => FeatureItem(feature: e))
+                              .toList())))
+            ],
           )
+
+
         ]),
       ),
       )
