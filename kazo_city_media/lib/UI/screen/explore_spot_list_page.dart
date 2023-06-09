5a4e18299fb894a0cd5237c40c0faa906b156414
shuya <akagisrapid@gmail.com>
2023-06-10 02:50:39 +0900

    おでかけスポットにtop_cardを入れる

--- kazo_city_media/lib/UI/screen/explore_spot_list_page.dart
+++ kazo_city_media/lib/UI/screen/explore_spot_list_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:kazo_city_media/UI/card/explore_spot_list_card.dart';
 
+import '../card/explore_spot_top_card.dart';
 import 'explore_spot_page.dart';
 
 class ExploreSpotListPage extends StatelessWidget {
@@ -63,8 +64,9 @@ class ExploreSpotListPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          const ExploreSpotCarouselCardItem(key: null),
           Expanded(
-              child: GridView.count(crossAxisCount: 2, children: spotItems))
+              child: GridView.count(crossAxisCount: 3, children: spotItems))
         ],
       ),
     );
