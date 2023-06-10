56f7a743e42b0ed7dea8aebe5ff8ee8c25a1cfab
shuya <akagisrapid@gmail.com>
2023-06-10 11:28:56 +0900

    google map api設定

--- kazo_city_media/ios/Runner/AppDelegate.swift
+++ kazo_city_media/ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyAPou7ZnF1Z_x4p2Kp4_GiTGHD7oPW9gbs")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
